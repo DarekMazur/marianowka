95863cf0e0d36a3ca332cde5e8b75ca2e97876b8
Darek Mazur <dev@darekmazur.it>
2026-08-25 17:08:42 +0200

    Create component for room data

---
+++ src/mocks/rooms.ts
@@ -0,0 +1,23 @@
+export const rooms = [
+	{
+		tag: "Domek Leśny",
+		title: "Chata pod Świerkiem",
+		desc: "Przestronny domek z kominkiem i tarasem widokowym na las.",
+		price: "350 zł",
+		image: "https://picsum.photos/seed/room1/600/400"
+	},
+	{
+		tag: "Pokój 2-osobowy",
+		title: "Pokój nad Jeziorem",
+		desc: "Przytulne wnętrze z widokiem na wschód słońca nad taflą wody.",
+		price: "220 zł",
+		image: "https://picsum.photos/seed/room2/600/400"
+	},
+	{
+		tag: "Apartament Rodzinny",
+		title: "Bocianie Gniazdo",
+		desc: "Idealny wybór dla rodziny z dziećmi, dwie sypialnie i aneks.",
+		price: "420 zł",
+		image: "https://picsum.photos/seed/room3/600/400"
+	}
+];
