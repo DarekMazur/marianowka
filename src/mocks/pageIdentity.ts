35aa32d1f8d9e865295d835931f52b3c6cecbc26
Darek Mazur <dev@darekmazur.it>
2026-08-28 00:32:16 +0200

    Divide Footer to Footer and Contact

--- src/mocks/pageIdentity.ts
+++ src/mocks/pageIdentity.ts
@@ -1,4 +1,5 @@
 export const pageIdentity = {
+	pageName: "Lorem Ipsum",
 	title: "Serce Suwalskiego Parku Krajobrazowego",
 	slogan: "Poczuj ciszę suwalskich jezior i lasów",
 	subSlogan: "Komfortowe domki i pokoje otoczone nietkniętą przyrodą. Miejsce, gdzie czas zwalnia swój bieg.",
