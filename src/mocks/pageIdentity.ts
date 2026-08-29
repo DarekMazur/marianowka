1c10d59f2d48c8fa315779955cd676d7412a6d94
Darek Mazur <dev@darekmazur.it>
2026-08-29 14:28:27 +0200

    Add translation to Hero mocks

--- src/mocks/pageIdentity.ts
+++ src/mocks/pageIdentity.ts
@@ -1,7 +1,26 @@
-export const pageIdentity = {
-	pageName: "Lorem Ipsum",
-	title: "Serce Suwalskiego Parku Krajobrazowego",
-	slogan: "Poczuj ciszę suwalskich jezior i lasów",
-	subSlogan: "Komfortowe domki i pokoje otoczone nietkniętą przyrodą. Miejsce, gdzie czas zwalnia swój bieg.",
-	heroImage: "https://picsum.photos/seed/1920/1080"
-}
+export const pageIdentityData = {
+  pl: {
+    pageName: 'Lorem Ipsum',
+    title: 'Serce Suwalskiego Parku Krajobrazowego',
+    slogan: 'Poczuj ciszę suwalskich jezior i lasów',
+    subSlogan:
+      'Komfortowe domki i pokoje otoczone nietkniętą przyrodą. Miejsce, gdzie czas zwalnia swój bieg.',
+    heroImage: 'https://picsum.photos/seed/1920/1080',
+  },
+  en: {
+    pageName: 'Lorem Ipsum',
+    title: 'The Heart of the Suwałki Landscape Park',
+    slogan: 'Feel the silence of Suwałki lakes and forests',
+    subSlogan:
+      'Comfortable cottages and rooms surrounded by untouched nature. A place where time slows down.',
+    heroImage: 'https://picsum.photos/seed/1920/1080',
+  },
+  ru: {
+    pageName: 'Lorem Ipsum',
+    title: 'Сердце Сувалкского ландшафтного парка',
+    slogan: 'Почувствуйте тишину сувалкских озер и лесов',
+    subSlogan:
+      'Комфортабельные домики и номера в окружении нетронутой природы. Место, где время замедляет свой бег.',
+    heroImage: 'https://picsum.photos/seed/1920/1080',
+  },
+} as const;
