1fdbd3ec40721acc5b47ff647b6e0807420c9dd4
Darek Mazur <dev@darekmazur.it>
2026-08-30 23:34:22 +0200

    Create mock images list

---
+++ src/mocks/homeSlides.ts
@@ -0,0 +1,52 @@
+export const homeSlides = [
+  {
+    image: 'https://picsum.photos/id/1040/1200/600',
+    alt: 'Dom agroturystyczny o poranku',
+    caption: 'Wypocznij z dala od miejskiego zgiełku',
+  },
+  {
+    image: 'https://picsum.photos/id/1069/1200/600',
+    alt: 'Lokalne zielone tereny',
+    caption: 'Odkryj uroki malowniczej okolicy',
+  },
+  {
+    image: 'https://picsum.photos/id/225/1200/600',
+    alt: 'Komfortowe wnętrza',
+    caption: 'Połączenie tradycji z nowoczesnym komfortem',
+  },
+  {
+    image: 'https://picsum.photos/id/292/1200/600',
+    alt: 'Domowe jedzenie',
+    caption: 'Smakuj tradycyjne, regionalne potrawy',
+  },
+  {
+    image: 'https://picsum.photos/id/319/1200/600',
+    alt: 'Aktywny wypoczynek',
+    caption: 'Ścieżki rowerowe i piesze w zasięgu ręki',
+  },
+  {
+    image: 'https://picsum.photos/id/431/1200/600',
+    alt: 'Wieczorne ognisko',
+    caption: 'Spędź miło czas pod rozgwieżdżonym niebem',
+  },
+  {
+    image: 'https://picsum.photos/id/1015/1200/600',
+    alt: 'Malownicze rzeki i jeziora',
+    caption: 'Idealne miejsca na relaks nad wodą',
+  },
+  {
+    image: 'https://picsum.photos/id/54/1200/600',
+    alt: 'Przytulny salon z kominkiem',
+    caption: 'Ciepła atmosfera w chłodniejsze wieczory',
+  },
+  {
+    image: 'https://picsum.photos/id/1059/1200/600',
+    alt: 'Lokalna fauna i flora',
+    caption: 'Bliski kontakt z nienaruszoną przyrodą',
+  },
+  {
+    image: 'https://picsum.photos/id/365/1200/600',
+    alt: 'Strefa relaksu w ogrodzie',
+    caption: 'Odpocznij na leżaku otoczonym zielenią',
+  },
+];
