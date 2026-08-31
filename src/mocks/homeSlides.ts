cd2cd66ee1fa82fdd3689d3d12d393ad62b584fc
Darek Mazur <dev@darekmazur.it>
2026-08-31 19:01:59 +0200

    Fix mocked images

--- src/mocks/homeSlides.ts
+++ src/mocks/homeSlides.ts
@@ -1,51 +1,61 @@
 export const homeSlides = [
   {
-    image: 'https://picsum.photos/id/1040/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Dom agroturystyczny o poranku',
     caption: 'Wypocznij z dala od miejskiego zgiełku',
   },
   {
-    image: 'https://picsum.photos/id/1069/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Lokalne zielone tereny',
     caption: 'Odkryj uroki malowniczej okolicy',
   },
   {
-    image: 'https://picsum.photos/id/225/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Komfortowe wnętrza',
     caption: 'Połączenie tradycji z nowoczesnym komfortem',
   },
   {
-    image: 'https://picsum.photos/id/292/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Domowe jedzenie',
     caption: 'Smakuj tradycyjne, regionalne potrawy',
   },
   {
-    image: 'https://picsum.photos/id/319/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1775152052928-d98d57c47887?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Aktywny wypoczynek',
     caption: 'Ścieżki rowerowe i piesze w zasięgu ręki',
   },
   {
-    image: 'https://picsum.photos/id/431/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1510312305653-8ed496efae75?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Wieczorne ognisko',
     caption: 'Spędź miło czas pod rozgwieżdżonym niebem',
   },
   {
-    image: 'https://picsum.photos/id/1015/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Malownicze rzeki i jeziora',
     caption: 'Idealne miejsca na relaks nad wodą',
   },
   {
-    image: 'https://picsum.photos/id/54/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Przytulny salon z kominkiem',
     caption: 'Ciepła atmosfera w chłodniejsze wieczory',
   },
   {
-    image: 'https://picsum.photos/id/1059/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1473448912268-2022ce9509d8?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Lokalna fauna i flora',
     caption: 'Bliski kontakt z nienaruszoną przyrodą',
   },
   {
-    image: 'https://picsum.photos/id/365/1200/600',
+    image:
+      'https://images.unsplash.com/photo-1584132967334-10e028bd69f7?auto=format&fit=crop&w=1200&h=600&q=80',
     alt: 'Strefa relaksu w ogrodzie',
     caption: 'Odpocznij na leżaku otoczonym zielenią',
   },
--- src/mocks/pageIdentity.ts
+++ src/mocks/pageIdentity.ts
@@ -5,7 +5,8 @@ export const pageIdentityData = {
     slogan: 'Poczuj ciszę suwalskich jezior i lasów',
     subSlogan:
       'Komfortowe domki i pokoje otoczone nietkniętą przyrodą. Miejsce, gdzie czas zwalnia swój bieg.',
-    heroImage: 'https://picsum.photos/seed/1920/1080',
+    heroImage:
+      'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=1920&h=1080&q=80',
   },
   en: {
     pageName: 'Lorem Ipsum',
@@ -13,7 +14,8 @@ export const pageIdentityData = {
     slogan: 'Feel the silence of Suwałki lakes and forests',
     subSlogan:
       'Comfortable cottages and rooms surrounded by untouched nature. A place where time slows down.',
-    heroImage: 'https://picsum.photos/seed/1920/1080',
+    heroImage:
+      'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=1920&h=1080&q=80',
   },
   ru: {
     pageName: 'Lorem Ipsum',
@@ -21,6 +23,7 @@ export const pageIdentityData = {
     slogan: 'Почувствуйте тишину сувалкских озер и лесов',
     subSlogan:
       'Комфортабельные домики и номера в окружении нетронутой природы. Место, где время замедляет свой бег.',
-    heroImage: 'https://picsum.photos/seed/1920/1080',
+    heroImage:
+      'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=1920&h=1080&q=80',
   },
 } as const;
